cec78c07fdc81e319bfc5263b7700a6ed8aa315c
abdel42 <abdel.madjid.brahimi@etu.univ-st-etienne.fr>
2019-02-25 16:34:07 +0100

    robot spring + salle + etablissement + correction typo template

--- src/main/java/com/targas/erp/dao/IEtudiantRepo.java
+++ src/main/java/com/targas/erp/dao/IEtudiantRepo.java
@@ -1,7 +1,11 @@
 package com.targas.erp.dao;
 
 import com.targas.erp.models.Etudiant;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface IEtudiantRepo extends CrudRepository<Etudiant,Integer> {
+
 }
